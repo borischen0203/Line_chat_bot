acb4369c028336624666987662c41a09bec3e130
PeiPeiC <l86426@gmail.com>
2023-07-26 21:43:47 +0800

    allowhost updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+j2f_kffd5oayx87)_1j9)_ja=+%q5_w)tz^5(is7u0v##7d6w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['6a3b-2001-b400-e209-c92c-1d2e-53de-ca91-62b2.ngrok-free.app','127.0.0.1']
+ALLOWED_HOSTS = ['cs-conversion-line-chatbot.onrender.com','6a3b-2001-b400-e209-c92c-1d2e-53de-ca91-62b2.ngrok-free.app','127.0.0.1']
 
 
 # Application definition
